6773e85f54cc846d3a6670afec0dbcdb89ea59e6
Kalin1n <kalinin_ivan_donetsk@ukr.net>
2021-01-25 00:24:28 +0300

    vuex mutations;

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,17 @@ export default createStore({
   state: {
     counter: 0
   },
-  mutations: {},
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 1;
+    },
+    decrement(state) {
+      state.counter = state.counter - 1;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    }
+  },
   actions: {},
   modules: {}
 });
--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -4,18 +4,28 @@
     <p>Value : {{ $store.state.counter }}</p>
     <button @click="add">Add</button>
     <button @click="subtract">Subtract</button>
+    <input type="number" placeholder="Number to add" v-model="value" />
+    <button @click="addTo">Add value</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "CounterPage",
+  data() {
+    return {
+      value: 0
+    };
+  },
   methods: {
     add() {
-      this.$store.state.counter++;
+      this.$store.commit("increment");
     },
     subtract() {
-      this.$store.state.counter--;
+      this.$store.commit("decrement");
+    },
+    addTo() {
+      this.$store.commit("increase", { value: Number(this.value) });
     }
   }
 };
